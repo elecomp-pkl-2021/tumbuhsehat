a3e9b510b8a29c4ed4cb26c18c101d5192eb244a
nrdnisml <nurudinismail69@gmail.com>
2021-07-12 10:00:26 +0700

    asu

--- application/views/components/footer.php
+++ application/views/components/footer.php
@@ -5,7 +5,8 @@
         <span class="text-center text-sm-left d-md-inline-block">Copyright © 2021 <span class="text-red">
                 Tumbuh Sehat. </span> All Rights
             Reserved.</span>
-        <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Dibuat oleh <a class="text-red" href="http://elecompindonesia.com" class="text-dark" target="_blank">Elecomp Software
+        <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Dibuat oleh <a class="text-red"
+                href="http://elecompindonesia.com" class="text-dark" target="_blank">Elecomp Software
                 House.</a></span>
     </div>
 </footer>
@@ -17,7 +18,8 @@
 <script src="<?= base_url() ?>assets/plugins/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="<?= base_url() ?>assets/plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
 <script src="<?= base_url() ?>assets/plugins/moment/moment.js"></script>
-<script src="<?= base_url() ?>assets/plugins/tempusdominus-bootstrap-4/build/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="<?= base_url() ?>assets/plugins/tempusdominus-bootstrap-4/build/js/tempusdominus-bootstrap-4.min.js">
+</script>
 <script src="<?= base_url() ?>assets/plugins/sweetalert2/sweetalert2.all.min.js"></script>
 <script src="<?= base_url() ?>assets/plugins/d3/dist/d3.min.js"></script>
 <script src="<?= base_url() ?>assets/plugins/c3/c3.min.js"></script>
@@ -29,21 +31,34 @@
 <script src="<?= base_url() ?>assets/js/layouts.js"></script>
 <script src="<?= base_url() ?>assets/js/datatables.js"></script>
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<!-- SELECT 2 PLUGINS-->
+<script src="<?= base_url('assets') ?>/plugins/select2/js/select2.full.min.js"></script>
 <script>
-    (function(b, o, i, l, e, r) {
-        b.GoogleAnalyticsObject = l;
-        b[l] || (b[l] =
-            function() {
-                (b[l].q = b[l].q || []).push(arguments)
-            });
-        b[l].l = +new Date;
-        e = o.createElement(i);
-        r = o.getElementsByTagName(i)[0];
-        e.src = 'https://www.google-analytics.com/analytics.js';
-        r.parentNode.insertBefore(e, r)
-    }(window, document, 'script', 'ga'));
-    ga('create', 'UA-XXXXX-X', 'auto');
-    ga('send', 'pageview');
+$(function() {
+    //Initialize Select2 Elements
+    $('.select2').select2();
+    $('.select2bs4').select2({
+        theme: 'bootstrap4'
+    });
+});
+</script>
+
+<!-- OUR SCRIPTS -->
+<script>
+(function(b, o, i, l, e, r) {
+    b.GoogleAnalyticsObject = l;
+    b[l] || (b[l] =
+        function() {
+            (b[l].q = b[l].q || []).push(arguments)
+        });
+    b[l].l = +new Date;
+    e = o.createElement(i);
+    r = o.getElementsByTagName(i)[0];
+    e.src = 'https://www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e, r)
+}(window, document, 'script', 'ga'));
+ga('create', 'UA-XXXXX-X', 'auto');
+ga('send', 'pageview');
 </script>
 <script type="text/javascript">
     $(document).ready(function() {
--- application/views/pages/home/index.php
+++ application/views/pages/home/index.php
@@ -5,10 +5,45 @@
                 <h3>Buat Janji Pemeriksaan</h3>
             </div>
             <div class="card-body">
-                <p>lolololo</p>
+                <form method="GET" action="">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-4 col-12">
+                                <label>Pilih Profil</label>
+                                <select class="select2bs4" name="profil" id="profil" style="width: 100%;">
+                                    <option><?= "Pilih Profil Pasien" ?></option>
+                                    <option><?= "isi data 1" ?></option>
+                                    <option><?= "isi data 2" ?></option>
+                                </select>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-12">
+                                <label>Pilih Cabang</label>
+                                <select class="select2bs4" name="cabang" id="cabang" style="width: 100%;">
+                                    <option><?= "Pilih Cabang Klinik" ?></option>
+                                    <option><?= "isi data 1" ?></option>
+                                    <option><?= "isi data 2" ?></option>
+                                </select>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-12">
+                                <label>Pilih Dokter</label>
+                                <select class="select2bs4" name="dokter" id="dokter" style="width: 100%;">
+                                    <option><?= "Pilih Dokter" ?></option>
+                                    <option><?= "isi data 1" ?></option>
+                                    <option><?= "isi data 2" ?></option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-6">
+                                <button type="submit" class="btn btn-primary">Cari Jadwal</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 </div>
 
 <div class="row">
