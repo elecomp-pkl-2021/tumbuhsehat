4c12085d1d141af7b55e93aa6f21409122391737
nrdnisml <nurudinismail69@gmail.com>
2021-07-13 09:15:04 +0700

    edit view home resepsionis

--- application/views/pages/home/resepsionis/index.php
+++ application/views/pages/home/resepsionis/index.php
@@ -5,51 +5,56 @@
                 <h3>Buat Janji Pemeriksaan</h3>
             </div>
             <div class="card-body">
-                <div class="col-md-3">
+                <form method="GET" action="">
                     <div class="form-group">
-                        <label for="exampleSelectGender">Cari Akun Keluarga <i class="fa fa-search"></i></label>
-                        <select class="js-example-basic-single form-control" id="exampleSelectGender" onchange="pilih_pasien(this.value)">
-                            <option value="-">Pilih Data Keluarga</option>
-                            <?php
-                            foreach ($keluarga as $k) {
-                            ?>
-                                <option value="<?= $k->id_user ?>"><?= $k->nama_depan_u . ' ' .  $k->nama_belakang_u . ' - ' . $k->no_hp ?></option>
-                            <?php
-                            }
-                            ?>
-                        </select>
-                        <div id="data_pasien">
-
+                        <div class="row">
+                            <div class="col-md-6 col-12">
+                                <label>Cari Akun Keluarga</label>
+                                <select class="select2bs4" name="akun_klg" id="akun_klg" style="width: 100%;"
+                                    onchange="pilih_pasien(this.value)">
+                                    <option><?= "Pilih Akun Keluarga" ?></option>
+                                    <?php foreach ($keluarga as $k) : ?>
+                                    <option value="<?= $k->id_user ?>">
+                                        <?= $k->nama_depan_u . ' ' .  $k->nama_belakang_u . ' - ' . $k->no_hp ?>
+                                    </option>
+                                    <?php endforeach;?>
+                                </select>
+                            </div>
+                            <div class="col-md-6 col-12">
+                                <label>Pilih Profil</label>
+                                <select class="select2bs4" name="profil" id="profil" style="width: 100%;">
+                                    <option><?= "Pilih Profil Pasien" ?></option>
+                                    <option><?= "isi data 1" ?></option>
+                                    <option><?= "isi data 2" ?></option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row my-3">
+                            <div class="col-md-6 col-12">
+                                <label>Pilih Cabang</label>
+                                <select class="select2bs4" name="cabang" id="cabang" style="width: 100%;">
+                                    <option><?= "Pilih Cabang Klinik" ?></option>
+                                    <option><?= "isi data 1" ?></option>
+                                    <option><?= "isi data 2" ?></option>
+                                </select>
+                            </div>
+                            <div class="col-md-6 col-12">
+                                <label>Pilih Dokter</label>
+                                <select class="select2bs4" name="dokter" id="dokter" style="width: 100%;">
+                                    <option><?= "Pilih Dokter" ?></option>
+                                    <option><?= "isi data 1" ?></option>
+                                    <option><?= "isi data 2" ?></option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-6">
+                                <button type="submit" class="btn btn-primary">Cari Jadwal</button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
 </div>
-
-<script type="text/javascript">
-    var linke = "<?php echo base_url() ?>";
-
-    function pilih_pasien(id) {
-        if (id == "-") {
-            $('#data_pasien').html("");
-        } else {
-            $.ajax({
-                type: 'GET',
-                url: `<?= base_url() ?>Pasien/getDataPasien/${id}`,
-                dataType: 'json',
-                success: (hasil) => {
-                    let isi = '';
-                    hasil.forEach(function(item) {
-                        console.log(item.alamat);
-                        isi += `<div class="alert bg-primary alert-primary text-white" role="alert">
-                                          ${item.nama_depan} ${item.nama_belakang} - ${item.alamat} 
-                                        </div>`
-                    });
-                    $('#data_pasien').html(isi);
-                }
-            });
-        }
-    }
-</script>
