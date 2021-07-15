7f6d8f514fa47c0364fa069f964a5bcab335a1dd
nrdnisml <nurudinismail69@gmail.com>
2021-07-15 10:00:18 +0700

    add controller pemeriksaan

--- application/views/pages/home/resepsionis/buat-jadwal.php
+++ application/views/pages/home/resepsionis/buat-jadwal.php
@@ -89,16 +89,53 @@
 
                             <div id="data-diri-part" class="content" role="tabpanel"
                                 aria-labelledby="data-diri-part-trigger">
-                                <div class="form-group">
-                                    <div class="row">
+
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="inputEmail4">Email</label>
+                                        <input type="email" class="form-control" id="inputEmail4">
                                     </div>
-                                    <div class="my-3">
-                                        <button type="button" class="btn btn-secondary btn-prev-form"
-                                            onclick="stepper.previous()">Kembali</button>
-                                        <button type="button" class="btn btn-primary btn-next-form"
-                                            onclick="stepper.next()">Selanjutnya</button>
+                                    <div class="form-group col-md-6">
+                                        <label for="inputPassword4">Password</label>
+                                        <input type="password" class="form-control" id="inputPassword4">
                                     </div>
                                 </div>
+                                <div class="form-group">
+                                    <label for="inputAddress">Address</label>
+                                    <input type="text" class="form-control" id="inputAddress"
+                                        placeholder="1234 Main St">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputAddress2">Address 2</label>
+                                    <input type="text" class="form-control" id="inputAddress2"
+                                        placeholder="Apartment, studio, or floor">
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="inputCity">City</label>
+                                        <input type="text" class="form-control" id="inputCity">
+                                    </div>
+                                    <div class="form-group col-md-4">
+                                        <label for="inputState">State</label>
+                                        <select id="inputState" class="form-control">
+                                            <option selected>Choose...</option>
+                                            <option>...</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                        <label for="inputZip">Zip</label>
+                                        <input type="text" class="form-control" id="inputZip">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" id="gridCheck">
+                                        <label class="form-check-label" for="gridCheck">
+                                            Check me out
+                                        </label>
+                                    </div>
+                                </div>
+
                             </div>
 
                             <div id="keluhan-part" class="content" role="tabpanel"
