e4b047f8ab8275a2892e36404560be7f3a414e08
martinakhusna <martinamanu21@gmail.com>
2021-07-19 16:01:50 +0700

    TAMPILAN REGISTER SELESAI CEUNAH

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -20,6 +20,20 @@ class Auth_model extends CI_Model
             return false;
         }
     }
+
+    public function register(){
+        $data = [
+            "nama_depan_u" => $this->input->post('nama_depan_u', true),
+            "nama_belakang_u" => $this->input->post('nama_belakang_u', true),
+            "tanggal_lahir" => $this->input->post('tanggal_lahir', true),
+            "jenis_kelamin" => $this->input->post('jenis_kelamin', true),
+            "alamat" => $this->input->post('alamat', true),
+            "email" => $this->input->post('email', true),
+            "no_hp" => $this->input->post('no_hp', true),
+            "password" => $this->input->post('password', true)
+        ];
+        $this->db->insert('login_session', $data);
+    }
 }
 
 /* End of file Login_model.php */
