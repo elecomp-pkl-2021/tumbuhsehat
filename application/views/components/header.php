532fd060347fe60409b40f94ac21a23888139621
Ardan Anjung Kusuma <ardananjungkusuma@gmail.com>
2021-08-04 15:07:35 +0700

    Ardan branch (#22)
    * Adding UI Filter jadwal home
    
    * Filter Cek jadwal Resepsionis(Home)
    
    * Fix UI Jadwal
    
    * Styling UI Jadwal and fix feature daftar
    
    * daftar pasien dokter
    
    * fix conflict
    
    * odontogram rusak
    
    * Fix layout odontogram
    
    Co-authored-by: nrdnisml <nurudinismail69@gmail.com>

--- application/views/components/header.php
+++ application/views/components/header.php
@@ -8,91 +8,87 @@ $hari = date('l ');
 <html class="no-js" lang="en">
 
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title><?= $title ?></title>
-	<meta name="description" content="">
-	<meta name="keywords" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title><?= $title ?></title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
 
-	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
 
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-	<link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/plugins/bootstrap/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/plugins/fontawesome-free/css/all.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/plugins/icon-kit/dist/css/iconkit.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/plugins/ionicons/dist/css/ionicons.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/plugins/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/plugins/jvectormap/jquery-jvectormap.css">
-	<link rel="stylesheet"
-		href="<?= base_url() ?>assets/plugins/tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/plugins/weather-icons/css/weather-icons.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/plugins/c3/c3.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/plugins/owl.carousel/dist/assets/owl.carousel.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/plugins/owl.carousel/dist/assets/owl.theme.default.min.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/dist/css/theme.min.css">
-	<script src="<?= base_url() ?>assets/src/js/vendor/modernizr-2.8.3.min.js"></script>
-	<script src="<?= base_url() ?>assets/js/time.js"></script>
-	<!-- Select2 Plugins -->
-	<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/select2/css/select2.min.css">
-	<link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-	<!-- STEPPER -->
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bs-stepper/dist/css/bs-stepper.min.css">
-	<!-- OUR CSS -->
-	<link rel="stylesheet" href="<?= base_url() ?>assets/css/radioInput.css">
-	<link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/icon-kit/dist/css/iconkit.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/ionicons/dist/css/ionicons.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/jvectormap/jquery-jvectormap.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/weather-icons/css/weather-icons.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/c3/c3.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/owl.carousel/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/owl.carousel/dist/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/dist/css/theme.min.css">
+    <script src="<?= base_url() ?>assets/src/js/vendor/modernizr-2.8.3.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/time.js"></script>
+    <!-- DROPIFY -->
+    <link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/dropify/dist/css/dropify.min.css">
+    <!-- Select2 Plugins -->
+    <link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/select2/css/select2.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <!-- STEPPER -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bs-stepper/dist/css/bs-stepper.min.css">
+    <!-- OUR CSS -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/radioInput.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/odontogram.css">
 </head>
 
 <body>
-	<div class="wrapper">
-		<header class="header-top" header-theme="light">
-			<div class="container-fluid">
-				<div class="d-flex justify-content-between">
-					<div class="top-menu d-flex align-items-center">
-						<button type="button" class="btn-icon mobile-nav-toggle d-lg-none"><span></span></button>
-						<button class="btn btn-light"><i class="ik ik-calendar mr-2"></i> <b><?= $hari ?></b>,
-							<?= $tanggal ?></button>
-						<button class="btn btn-light mr-2 ml-2" id="clock"></button>
-						<button class="btn btn-light mr-2"> <i class="ik ik-map-pin mr-2"></i> <b> Escalade Dental -
-								Tebet </b></button>
-						<?php if ($this->session->userdata("level") == "Klinik") : ?>
-						<button class="btn btn-light"> <i class="ik ik-user mr-2"></i> <b>Frontliner</b> </button>
-						<?php elseif ($this->session->userdata("level") == "Owner") : ?>
-						<button class="btn btn-light"> <i class="ik ik-user mr-2"></i> <b>Owner</b> </button>
-						<?php elseif ($this->session->userdata("level") == "Superadmin") : ?>
-						<button class="btn btn-light"> <i class="ik ik-user mr-2"></i> <b>Super Admin</b> </button>
-						<?php else : ?>
-						<button class="btn btn-light"> <i class="ik ik-user mr-2"></i>
-							<b><?= $this->session->userdata('nama_dokter'); ?></b> </button>
-						<?php endif ?>
-					</div>
-					<div class="top-menu d-flex align-items-center mr-20">
-						<div class="dropdown">
-							<a class="dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-								aria-haspopup="true" aria-expanded="false"><img class="avatar"
-									src="<?= base_url('assets') ?>/img/user.png" alt=""></a>
-							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-								<a class="dropdown-item" href="<?= base_url('auth/logout') ?>"><i
-										class="ik ik-power dropdown-icon"></i>
-									Logout</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
-		<div class="page-wrap">
-			<div class="app-sidebar colored">
-				<div class="sidebar-header">
-					<a class="header-brand" href="<?= base_url('home') ?>">
-						<!-- <div class="logo-img">
+    <div class="wrapper">
+        <header class="header-top" header-theme="light">
+            <div class="container-fluid">
+                <div class="d-flex justify-content-between">
+                    <div class="top-menu d-flex align-items-center">
+                        <button type="button" class="btn-icon mobile-nav-toggle d-lg-none"><span></span></button>
+                        <button class="btn btn-light"><i class="ik ik-calendar mr-2"></i> <b><?= $hari ?></b>,
+                            <?= $tanggal ?></button>
+                        <button class="btn btn-light mr-2 ml-2" id="clock"></button>
+                        <button class="btn btn-light mr-2"> <i class="ik ik-map-pin mr-2"></i> <b> Escalade Dental -
+                                Tebet </b></button>
+                        <?php if ($this->session->userdata("level") == "Klinik") : ?>
+                            <button class="btn btn-light"> <i class="ik ik-user mr-2"></i> <b>Frontliner</b> </button>
+                        <?php elseif ($this->session->userdata("level") == "Owner") : ?>
+                            <button class="btn btn-light"> <i class="ik ik-user mr-2"></i> <b>Owner</b> </button>
+                        <?php else : ?>
+                            <button class="btn btn-light"> <i class="ik ik-user mr-2"></i>
+                                <b><?= $this->session->userdata('nama_dokter'); ?></b> </button>
+                        <?php endif ?>
+                    </div>
+                    <div class="top-menu d-flex align-items-center mr-20">
+                        <div class="dropdown">
+                            <a class="dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="avatar" src="<?= base_url('assets') ?>/img/user.png" alt=""></a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="<?= base_url('auth/logout') ?>"><i class="ik ik-power dropdown-icon"></i>
+                                    Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="page-wrap">
+            <div class="app-sidebar colored">
+                <div class="sidebar-header">
+                    <a class="header-brand" href="<?= $this->session->userdata("level") == "Dokter" ? base_url('home/dokter') : base_url('home') ?>">
+                        <!-- <div class="logo-img">
                             <img src="<?= base_url('assets') ?>/src/img/logo-escalade.png" class="header-brand-img" alt="lavalite">
                         </div> -->
-						<span class="text">Tumbuh Sehat</span>
-					</a>
-				</div>
+                        <span class="text">Tumbuh Sehat</span>
+                    </a>
+                </div>
--- application/views/pages/home/dokter/index.php
+++ application/views/pages/home/dokter/index.php
@@ -1,41 +1,46 @@
 <div class="row">
     <div class="col">
         <div class="card">
-            <div class="card-header justify-content-between">
+            <div class="card-header justify-content-between bg-light">
                 <h3>Daftar Jadwal Pemeriksaan Pasien Anda</h3>
+                <div class="row align-items-center">
+                    <div class="col-2 text-right">
+                        <span class="font-weight-bold">Filter Data :</span>
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" id="filter-kode-booking" placeholder="Kode Booking" oninput="search_press()" autocomplete="off">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" id="filter-jam" placeholder="Jam Pemeriksaan" oninput="search_press()" autocomplete="off">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" id="filter-nama" placeholder="Nama Pasien" oninput="search_press()" autocomplete="off">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" id="filter-tgl-lahir" placeholder="Tanggal Lahir" oninput="search_press()" autocomplete="off">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" id="filter-status" placeholder="Status Booking" oninput="search_press()" autocomplete="off">
+                    </div>
+                </div>
             </div>
             <div class="card-body">
+
                 <div class="mx-5">
-                    <table class="table" id="data_table">
+                    <table class="table" id="data-table">
                         <thead>
                             <tr>
                                 <th>#</th>
                                 <th>Jam Periksa</th>
                                 <th>Nama</th>
                                 <th>Tanggal Lahir</th>
+                                <th>ID Booking</th>
                                 <th>No. Rekam Medis</th>
                                 <th>Status</th>
                                 <th class="nosort">&nbsp;</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>09.00 - 10.00</td>
-                                <td>Pasha Anisa</td>
-                                <td>12 Juni 1990</td>
-                                <td>Y2YxZDRhZTQ4</td>
-                                <td>-</td>
-                                <td>
-                                    <div class="table-actions text-center">
-                                        <a href="<?= base_url('dokter/pemeriksaan')?>">
-                                            <button class="btn btn-danger">Mulai
-                                                Pemeriksaan
-                                            </button>
-                                        </a>
-                                    </div>
-                                </td>
-                            </tr>
+                        <tbody id="table-result">
                         </tbody>
                     </table>
                 </div>
