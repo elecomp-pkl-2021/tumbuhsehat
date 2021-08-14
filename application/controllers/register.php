162745b4c6bc3d76cb5f232fd2f04241f36ed35d
Martin Amanu <56151818+martinakhusna@users.noreply.github.com>
2021-08-14 19:29:35 +0700

    Martin branch (#32)
    * AHAY
    
    * BARU
    
    * BARU
    
    * BARUPOLL

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -1,18 +1,16 @@
-<?php 
+<?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class register extends CI_Controller {
+class register extends CI_Controller
+{
 
     public function index()
     {
         $this->load->view('components/header_auth');
         $this->load->view('pages/auth/register');
         $this->load->view('components/footer_auth');
-        
     }
-
 }
 
 /* End of file Login.php */
-
