abd9fdd41bd203fb3be40080d6e494c369337571
sugengprastiyo <tiyo.sugeng@yahoo.com>
2021-08-24 12:53:47 +0700

    requirement buat akun keluarga dan tambah pasien

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -28,53 +28,30 @@ class Home extends CI_Controller
             <li class="breadcrumb-item active"><i class="ik ik-home"></i></li>';
 
         $data['keluarga'] = $this->Klinik_model->tampil();
+
         $data['dokter'] = $this->Dokter_model->get_dokter();
         $data['cabang'] = $this->Cabang_model->get_cabang();
         $data['rencana_sebelum'] = $this->Rencana_model->get_rencana_sebelum();
-
         $this->load->view('components/header', $data);
 
         if ($this->session->userdata('level') == "Owner") {
             $this->load->view('components/sidebar_owner');
         } elseif ($this->session->userdata('level') == "Klinik") {
             $this->load->view('components/sidebar_resepsionis');
-        } elseif ($this->session->userdata('level') == "Superadmin") {
-            $this->load->view('components/sidebar_superadmin');
-        } else{
+        } else {
             $this->load->view('components/sidebar_dokter');
         }
-        
+
         $this->load->view('components/breadcrumbs', $data);
-        if ($this->session->userdata('level') != "Dokter") {
+
+        if ($this->session->userdata('level') == "Owner" || $this->session->userdata('level') == "Klinik") {
             $this->load->view('pages/home/resepsionis/index', $data);
-        } 
+        } else {
+            $this->load->view('pages/home/dokter/index');
+        }
+
         $this->load->view('components/footer');
         // end resepsionis
-    }
-    function buatAkunKeluarga()
-    {
-        $data['title'] = "Home | Tumbuh Sehat";
-        $data['judulHalaman'] = "Buat Akun Keluarga";
-        $data['subJudulHalaman'] = "Selamat datang di <b>Tumbuh Sehat</b>";
-        $data['iconHalaman'] = "ik ik-edit bg-blue";
-        $data['breadcrumbs'] = '
-            <li class="breadcrumb-item"><a href="'.base_url('home').'"><i class="ik ik-home"></i></a></li>
-            <li class="breadcrumb-item active">Buat Akun Keluarga</li>';
-
-      $data['_antrian'] = 1;
-      $data['content'] = 'klinik/tambah_pasien';
-      $this->load->view('components/header', $data);
-      $this->load->view('components/sidebar_resepsionis');
-      $this->load->view('components/breadcrumbs', $data);
-      $this->load->view('pages/home/resepsionis/buatAkunKeluarga', $data);
-      $this->load->view('components/footer');
-  
-      // if($this->input->post('kirim'))
-      // {
-      //   $nama_depan = $this->input->post('nama_depan');
-      //   $
-      // }
-  
     }
 }
 
